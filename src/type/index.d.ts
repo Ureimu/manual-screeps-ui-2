f6070edb9018ff54ab8624f3e0b29c38e410ab72
Ureimu <steam1090693441@163.com>
2026-01-29 16:41:25 +0800

    Update test data and add JSDoc comments to type definitions

--- src/type/index.d.ts
+++ src/type/index.d.ts
@@ -53,10 +53,19 @@ declare module "frame/spawn/spawning/readyCondition/type" {
 declare module "frame/spawn/spawnPool/type" {
     import { ReadyCondition } from "frame/spawn/spawning/readyCondition/type";
     export interface SpawnCreepDetail {
+        /**
+         * creep的名称。
+         */
         creepName: string;
+        /**
+         * （未启用）shiftCreep组的id列表
+         */
         idList: {
             [name: number]: boolean;
         };
+        /**
+         * （未启用）shiftCreep的等级。
+         */
         creepLevel?: number;
         /**
          * creep的部件设定名称
@@ -66,21 +75,48 @@ declare module "frame/spawn/spawnPool/type" {
          * creep的部件字符串
          */
         creepBody: string;
+        /**
+         * creep的孵化优先级。
+         */
         priority: number;
+        /**
+         * creep的孵化条件。
+         */
         spawnCondition: readyConditionKey;
+        /**
+         * creep的当前情况。分为alive:活着，queue:队列中和dead:死亡。
+         */
         creepCondition: CreepCondition;
+        /**
+         * creep的当前孵化池情况。分为running:在孵化池等待孵化，ready:等待进入孵化池，notReady:未准备进入孵化池。
+         */
         state: runningState;
+        /**
+         * shiftCreep的控制孵化的函数名称。
+         */
         subCond?: string;
+        /**
+         * （未启用）shiftCreep的控制孵化的函数参数。
+         */
         subCondArgs?: string[];
+        /**
+         * creep上次孵化时所在的spawn名称。
+         */
         spawnName?: string;
+        /**
+         * creep是否已正在spawn中孵化。
+         */
         spawning?: boolean;
+        /**
+         * creep孵化时所属房间名称。
+         */
         roomName: string;
+        /**
+         * creep出生次数。
+         */
         spawnCount: number;
         /**
-         * 从属的project名称。
-         *
-         * @type {string}
-         * @memberof SpawnCreepDetail
+         * creep从属的project名称。
          */
         projectName?: string;
     }
@@ -132,12 +168,43 @@ declare module "utils/TimeSeriesData/type" {
             : undefined;
     }
 }
+declare module "AI/AIUreium/control/outwardsSource/type" {
+    export interface RoomStatusOutwardsSource {
+        lastRunTime: number;
+        isRunning: boolean;
+        sources: {
+            [name: string]: {
+                isInUse: boolean;
+                isChosen: boolean;
+                isRemoved: boolean;
+                reason: string;
+            };
+        };
+    }
+    export interface RoomStatusData {
+        outwardsSource?: RoomStatusOutwardsSource;
+    }
+}
 declare module "AI/AIUreium/ui/type" {
     import { creepBodyConfigDetail } from "frame/creep/body/type";
     import { CreepGroupMemory, CreepGroupMode } from "frame/creep/group/type";
     import { SpawnCreepDetail } from "frame/spawn/spawnPool/type";
     import { ErrorSegmentMemory } from "utils/ErrorMapper/type";
     import { SingleData, SingleTypedTreeDataRecord } from "utils/TimeSeriesData/type";
+    import { RoomStatusData } from "AI/AIUreium/control/outwardsSource/type";
+    export { ErrorSegmentMemory };
+    /**
+     * 该文件为ui库与本代码库共享的类型文件。
+     *
+     * 在添加新类型时，需要保证引入的文件不应包含：
+     *
+     * 1. 全局类型声明，如declare global。
+     * 2. 其他的导入依赖，除非该依赖也满足当前的要求。
+     *
+     * 建议保证导入的文件只包含类型，以避免问题。
+     *
+     * 如果导入的文件包含全局类型声明，可以把全局类型声明放到其他文件。
+     */
     export interface OriginScreepsData {
         type: "OriginScreepsData";
         timeSeriesData: TimeSeriesStats<(number | null)[]> & {
@@ -206,6 +273,7 @@ declare module "AI/AIUreium/ui/type" {
         spawnPool: {
             [creepName: string]: SpawnCreepDetail;
         };
+        status?: RoomStatusData;
     }
     export type TimeSeriesStats<T extends string | number | (number | null)[]> = {
         userData: {
